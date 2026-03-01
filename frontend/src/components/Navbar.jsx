ab17264b51c242dba7a6a9f3c9b027388c8d0d23
Suji V <Suji26262@gmail.com>
2026-03-01 20:56:00 +0530

    change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -57,6 +57,7 @@ const Navbar = () => {
       {/* Sidebar menu for small screens */}
       <div className={`absolute top-0 bottom-0 overflow-hidden bg-white  transition-all ${visible ? 'w-full'  : 'w-0'}`}>
         <div className="flex flex-col text-gray-600">
+          
           <div onClick={()=>setVisible(false)} className="flex items-center gap-4 p-3 cursor-pointer">
             <img src={assets.dropdown_icon} className="h-4 rotate-180" alt="" />
             <p>Back</p>
