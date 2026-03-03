bc46284a1bb5b5acc29ca8aa51cc9cff971c6a11
Suji V <Suji26262@gmail.com>
2026-03-03 20:24:03 +0530

    create a hero section

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border border-gray-400'>
+      {/* Hero Left Side */}
+      <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'></div>
+      <div className='text-[#414141]'>
+        <div className='flex items-center gap-2'>
+            <p className='w-8 md'></p>
+            <p></p>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Hero
