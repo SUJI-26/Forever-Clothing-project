64151d009c4f1824ff01723e16ed724b74692e13
Suji V <Suji26262@gmail.com>
2026-03-05 10:46:10 +0530

    Finish Bestseller componet section

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -3,38 +3,44 @@ import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
 import ProductItem from './ProductItem'
 
-const LatestCollection = () => {
+const BestSeller = () => {
 
   const { products } = useContext(ShopContext)
-
-  const [latestProducts, setLatestProducts] = useState([])
+  const [bestSeller, setBestSeller] = useState([])
 
   useEffect(() => {
-    setLatestProducts(products.slice(0,10))
+
+    const bestProduct = products.filter((item) => item.bestseller)
+
+    setBestSeller(bestProduct.slice(0,5))
+
   }, [products])
 
   return (
     <div className='my-10'>
 
-      <div className='text-center py-8 text-3xl'>
-        <Title text1={'LATEST'} text2={'COLLECTION'} />
+      {/* Title Section */}
+      <div className='text-center text-3xl py-8'>
+
+        <Title text1={'BEST'} text2={'SELLERS'} />
 
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Iusto modi numquam odio totam ab. Natus!
+          Voluptates, explicabo eius. Iusto minima reiciendis quasi
+          saepe ducimus dolorum quibusdam aspernatur.
         </p>
+
       </div>
 
-      {/* Rendering Products */}
-
+      {/* Products Grid */}
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
 
-        {latestProducts.map((item) => (
+        {bestSeller.map((item) => (
           <ProductItem
             key={item._id}
             id={item._id}
-            image={item.image}
             name={item.name}
+            image={item.image}
             price={item.price}
           />
         ))}
@@ -45,4 +51,4 @@ const LatestCollection = () => {
   )
 }
 
-export default LatestCollection
+export default BestSeller
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LatestCollection from '../components/LatestCollection'
 import Hero from '../components/Hero'
+import BestSeller from '../components/BestSeller'
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
       
     </div>
   )
