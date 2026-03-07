2a1f7dbd0cbb873d280098041b9962aed770147c
Suji V <Suji26262@gmail.com>
2026-03-07 13:34:37 +0530

    Add a cart features in webpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,10 +13,12 @@ import Order from './pages/Order'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
 Hero
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
       <Navbar />
       <SearchBar/>
       <Routes>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -7,7 +7,7 @@ import { useContext, useState, useEffect } from "react";
 const Product = () => {
 
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency ,addToCart} = useContext(ShopContext);
 
   const [productData, setProductData] = useState(null);
   const [image, setImage] = useState("");
@@ -117,7 +117,7 @@ const Product = () => {
 
             </div>
 
-            <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+            <button onClick={()=>addToCart(productData._id,size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
               ADD TO CART
             </button>
 
